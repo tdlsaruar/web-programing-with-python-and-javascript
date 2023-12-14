3d2c08d0236f3cc99be97c4be377295ebb3683ce
tdlsaruar <saruarahmed321@gmail.com>
2023-12-14 11:12:53 -0600

    first day of new journey

---
+++ CS 50's introduction to programing with python/01_functions,variables/01_Hello.py
@@ -0,0 +1,2 @@
+# Demonstrates a function with a positional argument 2 3 
+print("hello, world")
---
+++ CS 50's introduction to programing with python/01_functions,variables/overall.py
@@ -0,0 +1,9 @@
+# input("Whhat's your name :")
+print("Welcome to python world after 2 years you are here that's nice!")
+# return values
+name =input("What's your name my dear?:")
+# Remove whitespace from str
+name = name.strip()
+# Capitalize user's name
+name = name.capitalize()
+print("welcome to real world ",name)
